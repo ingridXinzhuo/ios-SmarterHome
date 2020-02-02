e8a319d970860122890b53db52bae2bdeeb7ced7
于昕卓 <xinzhuoyu@us-macbook-puro.home>
2020-02-03 00:30:31 +1100

    smarter-home

---
+++ 5140AssignmentStarter/Controllers/ReferenceViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ReferenceViewController.swift
+//  5140AssignmentStarter
+//
+//  Created by 于昕卓 on 10/11/19.
+//  Copyright © 2019 XinzhuoYu. All rights reserved.
+//
+
+//import UIKit
+//
+//class ReferenceViewController: UIViewController {
+//
+//
+//    @IBOutlet weak var view1: UITextView!
+//
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        let attributedString = NSAttributedString.makeHyperlink(path: view1.text!, in: view1.text!, as: view1.text!)
+//        // Do any additional setup after loading the view.
+//        view1.attributedText = attributedString
+//
+//    }
+//
+//
+////    /*
+//    // MARK: - Navigation
+//
+//    // In a storyboard-based application, you will often want to do a little preparation before navigation
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        // Get the new view controller using segue.destination.
+//        // Pass the selected object to the new view controller.
+//    }
+//    */
+//
+//}
